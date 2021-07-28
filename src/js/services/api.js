bc3fb631c4c2d2ad599846a8e5bb5f6470c16a69
iggyfo <iggyfo@gmail.com>
2021-07-28 07:18:19 +0300

    Добавляет второй истточник курсов валют

--- src/js/services/api.js
+++ src/js/services/api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import {getCurrencyPairs} from "../utils";
 
 const BACKEND_URL = `https://currate.ru/api/?get=rates&pairs=${getCurrencyPairs()}&key=1b6e86515212c0793ddd73b4cd8708552`;
+// const BACKEND_URL = `https://api.currencylayer.com/live?access_key=f7246fd61d8438ca758e25e23e6400cd&currencies=AUD,EUR,GBP,PLN`
 const REQUEST_TIMEOUT = 5000;
 
 const HttpCode = {
